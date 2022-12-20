b1feb87d42b02b020c7a0aad2394363eb7a33568
medicalanthrax <carlotran4@gmail.com>
2022-12-19 22:42:24 -0600

    buy-alpaca

--- backtesting/simulate_buy.py
+++ backtesting/simulate_buy.py
@@ -3,6 +3,7 @@
 import datetime
 import requests
 from ratelimiter import RateLimiter
+from alpaca.data.historical import StockHistoricalDataClient
 
 
 @RateLimiter(max_calls=5, period=60)
@@ -31,4 +32,16 @@ def simulate_buy(ticker, date, period, amount, api_key="Y08A7DHDJ0Y1SL4G"):
         nearest_friday+datetime.timedelta(weeks=period))]["4. close"])
     return ((price_at_sell/price_at_buy)*amount)-amount
 
-def simulate_buy_alpaca():
+def simulate_buy_alpaca(ticker, date, period, amount):
+    """Same as simulate_buy, but using Alpaca API instead of Alphavantage.
+    :param ticker: String object representing the ticker of stock to be
+    simulated.
+    :param date: String object in YYYY-MM-DD format of date bought
+    :param period: Length in weeks to simulate holding the stock
+    :param amount: Float object representing the amount you want to buy of
+    said stock.
+    """
+    key = "PKUL8J8J58VKUVBXMSLY"
+    secret_key = "VX8VmhTirGC0sLrFf1X1TSwGiZ1yVKbfrRskTwWe"
+    stock_client = StockHistoricalDataClient(key,secret_key)
+    #https://alpaca.markets/docs/python-sdk/market_data.html#market-data
